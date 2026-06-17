6213beac7d54e4779edfb1084a52bdcfce9d3398
parchedAlbumen <kennethpamintuan2305@gmail.com>
2026-06-17 12:43:53 -0700

    quick

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
-from dotenv import load_dotenv
-import os 
+# from dotenv import load_dotenv
+# import os 
+import requests 
 
-load_dotenv()
-api_key = os.getenv("MY_API_KEY")
-print(api_key)
+# load_dotenv()
+# api_key = os.getenv("MY_API_KEY")
+# print(api_key)
+
+response = requests.get("https://api.github.com/users/octocat")
+print(response.status_code, "\n")
+data = response.json()
+print(f"name: {data["login"]}\nuser id is {data["id"]} ")
